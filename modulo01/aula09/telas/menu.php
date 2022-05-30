b0ccd057f6a2aea6e94fa87cd8287be95eaaf8d2
BRUNO APOSTOLICO <bapostolico@gmail.com>
2022-05-30 11:16:58 -0300

    Aula Funções

--- modulo01/aula09/telas/menu.php
+++ modulo01/aula09/telas/menu.php
@@ -2,5 +2,6 @@
 <a class="btn btn-outline-dark" href="/login">Login</a>
 <a class="btn btn-outline-dark" href="/cadastro">Cadastro</a>
 <a class="btn btn-outline-dark" href="/listar">Listar</a>
+<a class="btn btn-outline-dark" href="/relatorio">Relatórios</a>
 <a class="btn btn-outline-dark" href="/admin">Admin</a>
 <hr>
